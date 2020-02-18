7884f4ad0f571b23d63b918205316fd9261bbac2
Nery Galicia <nerygalicia3@gmail.com>
2020-02-17 21:11:53 -0600

    Se elaboro la API del proyecto y las consultas a la base de datos.

---
+++ MedikaAPI/routes/Paciente.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+router.use(bodyParser.json());
+router.use(cors());
+
+var Database = require('./Database.js');
+
+router.get('/ObtenerPaciente', function (req, res, next) {
+
+    var database = new Database();
+    var sql = 'SELECT * FROM PACIENTE';
+
+    database.query(sql)
+        .then(rows => {
+            res.send(rows[0]);
+            return database.close();
+        }, err => {
+            return database.close().then(() => {
+                throw err;
+            })
+        })
+        .catch(err => {
+            console.log(err);
+            res.send({
+                mensaje: 'Se produjo un error al obtener los pacientes.'
+            });
+        });
+
+});
+
+module.exports = router;
