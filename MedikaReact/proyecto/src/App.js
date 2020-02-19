55e1072c72fb69a1504b89ac408417aecc0c6936
Diana Jimenez <dianajimalon@gmail.com>
2020-02-19 15:16:55 -0600

    Actualizacion de Menu

--- MedikaReact/proyecto/src/App.js
+++ MedikaReact/proyecto/src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './App.css';
+import logo from './imagenes/fondoMedika.png';
 import SimpleMenu from './Componente/SimpleMenu';
+import PermanentDrawerLeft from './Componente/PermantDrawerLeft';
 
 function App() {
   return (
     <div className="App">
-      <section className="componentes" align="left">
-        <SimpleMenu />
-      </section>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />  
+      </header>
+      <body bgcolor="#008b8b">
+        <section className="App-Menu" align="left">
+          <SimpleMenu />
+        </section>
+        <section>
+          <PermanentDrawerLeft />
+        </section>
+        <section className="App-Cuerpo">
+
+        </section>
+        
+      </body>
+      
     </div>
   );
 }
