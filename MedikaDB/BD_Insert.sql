9dc28b1fdb9c658e65db450c9c9e0854bcb1aff2
Diana Jimenez <dianajimalon@gmail.com>
2020-02-21 18:27:23 -0600

    BD Scripts
    Modificacion de script de bd

---
+++ MedikaDB/BD_Insert.sql
@@ -0,0 +1,5 @@
+USE MedikaDB;
+-- insert en Empelado
+insert into EMPLEADO(nombre, apellido, dpi, rol, telefono, correo) values('Noah Fernando', 'Cen Torres', '1234567891011','GERENTE', '234567890', 'noahCen@gmail,com');
+-- insert en Medicamento
+insert into MEDICINA(nombre, cantidad, distribuidora)  values ('VitaPirena',12,'Bayer Inc');
---
+++ MedikaDB/DB_Medika.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE MedikaDB;
+USE MedikaDB;
+-- si ya esta creada la base de datos
+DROP TABLE PACIENTE;
+DROP TABLE MEDICINA;
+DROP TABLE EMPLEADO;
+-- fin drop
+
+CREATE TABLE MEDICINA (
+    nombre         VARCHAR(100),
+    cantidad       INTEGER,
+    distribuidora  VARCHAR(100),
+    codigo         INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT
+);
+
+CREATE TABLE EMPLEADO(
+    nombre    VARCHAR(100),
+    apellido  VARCHAR(100),
+    dpi       VARCHAR(13),
+    rol       VARCHAR(100),
+    telefono  VARCHAR(12),
+    correo    VARCHAR(50),
+    cod       INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT
+);
+
+select * from EMPLEADO;
+select * from MEDICINA;
